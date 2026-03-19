af4a9f9f5104cf5ddb8d103659fe94d6aeac30df
thanos <thanosv@gmail.com>
2026-03-19 07:28:52 -0400

    removed emoji

--- demo/config/runtime.exs
+++ demo/config/runtime.exs
@@ -9,6 +9,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :demo, DemoWeb.Endpoint,
+    server: true,
     url: [host: host, port: 443, scheme: "https"],
     http: [ip: {0, 0, 0, 0, 0, 0, 0, 0}, port: port],
     secret_key_base: secret_key_base
